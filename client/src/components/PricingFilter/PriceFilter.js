7329f31d9c5a0201b8dde56ea4bba55ca9a0284e
Alex <alexgofresh@gmail.com>
2024-02-14 23:00:32 +0100

    onchange selection added

--- client/src/components/PricingFilter/PriceFilter.js
+++ client/src/components/PricingFilter/PriceFilter.js
@@ -1,17 +1,26 @@
-import React from 'react';
 import styles from './PriceFilter.module.scss';
 import classNames from 'classnames';
+import React, { useState } from 'react';
 
 const PricingForm = () => {
+  const [selectValue, setSelectedValue] = useState('');
+
+  const handleSelectChange = ({ target: { value } }) => {
+    setSelectedValue(value);
+  };
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Pricing for</h2>
       <div className={styles.wrap}>
         <button className={styles.btn}>
-          <span>Name</span>
+          <span>{selectValue ? selectValue : 'Name'}</span>
           <i className={classNames('fa fa-angle-down', styles.selectIcon)}></i>
         </button>
-        <select className={styles.select} name='contest_types'>
+        <select
+          className={styles.select}
+          onChange={handleSelectChange}
+          name='contest_types'
+        >
           <option>Name</option>
           <option>Logo</option>
           <option>Tagline</option>
